6dea5b358ddde823b1defaa09b5b83cbddbff522
Andreas Kollegger <akollegger@gmail.com>
2025-10-11 13:40:03 +0100

    bumped tree-sitter-gram to 0.1.10

--- src/parser.c
+++ src/parser.c
@@ -3746,7 +3746,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_gram(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 9,
+      .patch_version = 10,
     },
   };
   return &language;
