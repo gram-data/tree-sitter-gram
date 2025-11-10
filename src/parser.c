eef2ec5d817245e02ca541437635a5982e6fb54f
Andreas Kollegger <akollegger@gmail.com>
2025-11-10 13:57:41 +0000

    Release 0.2.1 -- now with line comments (but not inline)

--- src/parser.c
+++ src/parser.c
@@ -5251,7 +5251,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_gram(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
