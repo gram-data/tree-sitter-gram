6aa2e5d18ed2e5374c4da2c80f2cdef5a03da332
Andreas Kollegger <akollegger@gmail.com>
2026-03-27 12:30:58 +0000

    quoted-names: more tweaking of highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -22,10 +22,31 @@
 (pattern_reference identifier: (_) @variable)
 
 ; Definition-like identifiers and labels (FR-001): @type
-; Direct identifier and labels highlighting
-(identifier: (_) @type)
+; Node and subject pattern identifiers (symbols, quoted names, integers in subject)
+(node_pattern subject: (symbol) @type)
+(node_pattern subject: (quoted_name) @type)
+(node_pattern subject: (integer) @type)
+(subject_pattern subject: (symbol) @type)
+(subject_pattern subject: (quoted_name) @type)
+(subject_pattern subject: (integer) @type)
+
+; Labels (symbols and quoted names)
 (labels (symbol) @type)
 (labels (quoted_name) @type)
+
+; Relationship arrow identifiers (in arrow brackets)
+(right_arrow subject: (symbol) @type)
+(right_arrow subject: (quoted_name) @type)
+(right_arrow subject: (integer) @type)
+(left_arrow subject: (symbol) @type)
+(left_arrow subject: (quoted_name) @type)
+(left_arrow subject: (integer) @type)
+(undirected_arrow subject: (symbol) @type)
+(undirected_arrow subject: (quoted_name) @type)
+(undirected_arrow subject: (integer) @type)
+(bidirectional_arrow subject: (symbol) @type)
+(bidirectional_arrow subject: (quoted_name) @type)
+(bidirectional_arrow subject: (integer) @type)
 
 ; Annotation identifiers and labels
 (identified_annotation identifier: (_) @type)
