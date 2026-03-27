869488b6bbea325b4c4153a7a6bee97df7931a58
Andreas Kollegger <akollegger@gmail.com>
2026-03-27 12:53:25 +0000

    quoted-names: highlight labels as @type

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -21,40 +21,20 @@
 ; Reference identifier: pattern_reference (FR-001)
 (pattern_reference identifier: (_) @variable)
 
-; Definition-like identifiers and labels (FR-001): @type
-; Node and subject pattern identifiers (symbols, quoted names, integers in subject)
-(node_pattern subject: (symbol) @type)
-(node_pattern subject: (quoted_name) @type)
-(node_pattern subject: (integer) @type)
-(subject_pattern subject: (symbol) @type)
-(subject_pattern subject: (quoted_name) @type)
-(subject_pattern subject: (integer) @type)
+; Symbols and identifiers (generic; definition/reference/tag captured above)
+(symbol) @variable
 
-; Labels (symbols and quoted names)
+; Definition-like identifiers and labels (FR-001): @type
+; These must come AFTER generic symbol rule to take priority
+
+; Labels (symbols and quoted names in labels nodes)
 (labels (symbol) @type)
 (labels (quoted_name) @type)
-
-; Relationship arrow identifiers (in arrow brackets)
-(right_arrow subject: (symbol) @type)
-(right_arrow subject: (quoted_name) @type)
-(right_arrow subject: (integer) @type)
-(left_arrow subject: (symbol) @type)
-(left_arrow subject: (quoted_name) @type)
-(left_arrow subject: (integer) @type)
-(undirected_arrow subject: (symbol) @type)
-(undirected_arrow subject: (quoted_name) @type)
-(undirected_arrow subject: (integer) @type)
-(bidirectional_arrow subject: (symbol) @type)
-(bidirectional_arrow subject: (quoted_name) @type)
-(bidirectional_arrow subject: (integer) @type)
 
 ; Annotation identifiers and labels
 (identified_annotation identifier: (_) @type)
 (identified_annotation labels: (labels (symbol) @type))
 (identified_annotation labels: (labels (quoted_name) @type))
-
-; Symbols and identifiers (generic; definition/reference/tag captured above)
-(symbol) @variable
 
 ; Keywords and operators
 [
